b46bcf8e3ada44a999685c5566e06ad2690108c1
Jan Wolff <janw@mailbox.org>
2020-03-15 18:22:53 +0100

    write objects into AMAP files

--- src/plugins/amap/amapplugin.cpp
+++ src/plugins/amap/amapplugin.cpp
@@ -5,6 +5,8 @@
 #include "savefile.h"
 #include "tile.h"
 #include "tilelayer.h"
+#include "objectgroup.h"
+#include "mapobject.h"
 
 #include <QCoreApplication>
 #include <QDir>
@@ -47,7 +49,7 @@ bool AmapPlugin::write(const Tiled::Map *map, const QString &fileName, Tiled::Fi
 
     SaveFile file(fileName);
 
-    if (!file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+    if (!file.open(QIODevice::WriteOnly)) {
         mError = QCoreApplication::translate("File Errors", "Could not open file for writing.");
         return false;
     }
@@ -55,15 +57,33 @@ bool AmapPlugin::write(const Tiled::Map *map, const QString &fileName, Tiled::Fi
     QDataStream data(file.device());
     data.setByteOrder(QDataStream::LittleEndian);
 
+    quint32 tileLayerCount = 0;
+    for(Layer *layer : map->layers()) if(layer->isTileLayer()) tileLayerCount++;
+
     data.writeRawData("AMAP", 4);
     data << quint8(VERSION);
-    data << quint32(map->width()) << quint32(map->height()) << quint32(map->layerCount());
+    data << quint32(map->width()) << quint32(map->height()) << tileLayerCount;
 
     std::vector<quint8> collisions(map->height() * map->width(), 0);
 
+    const ObjectGroup *objectGroup = nullptr;
+
+    // write tiles
+
     for(Layer *layer : map->layers()) {
-        if(!layer->isTileLayer()) continue;
-        const TileLayer *tileLayer = static_cast<const TileLayer*>(layer);
+        if(!layer->isTileLayer() && layer->isObjectGroup()) {
+            // since we iterate over all layers, might aswell use the time to extract the object layer here
+            if(objectGroup) {
+                mError = QCoreApplication::translate("AMAP Error", "More than one object group is not supported.");
+                return false;
+            }
+            objectGroup = layer->asObjectGroup();
+            continue;
+        } else if(!layer->isTileLayer()) {
+            mError = QCoreApplication::translate("AMAP Error", "Unsupported layer tile in map.");
+            return false;
+        }
+        const TileLayer *tileLayer = layer->asTileLayer();
 
         std::vector<qint16> layerdata(map->height() * map->width(), 0);
 
@@ -88,9 +108,32 @@ bool AmapPlugin::write(const Tiled::Map *map, const QString &fileName, Tiled::Fi
         data.writeRawData(compressedLayerdata.data(), compressedLayerdata.size());
     }
 
+    if(!objectGroup) {
+        mError = QCoreApplication::translate("AMAP Error", "You need exactly one object group in your map.");
+        return false;
+    }
+
+    // write collisions
+
     std::vector<char> compressedCollisions = compressVector(collisions);
     data << quint32(compressedCollisions.size());
     data.writeRawData(compressedCollisions.data(), compressedCollisions.size());
+
+    // write objects
+
+    data << quint32(objectGroup->objectCount());
+    for(MapObject *object : objectGroup->objects()) {
+        std::string name = object->name().toLatin1().toStdString();
+        if(name.length() > 8) {
+            mError = QCoreApplication::translate("AMAP Error", "Object name to long (max 8 Latin1 bytes).");
+            return false;
+        }
+        name.resize(8, '\0');
+        data.writeRawData(name.data(), name.length());
+
+        data << quint16(object->x() / map->tileSize().width());
+        data << quint16(object->y() / map->tileSize().height());
+    }
 
     file.commit();
     return true;
